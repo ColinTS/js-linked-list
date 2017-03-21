16bf413c05aaa3e2c13e3b00c5d9b07d2e428963
ctsygiel <ctsygiel@gmail.com>
2017-03-20 19:19:51 -1000

    Made remove and get tests pass

--- linkedList.js
+++ linkedList.js
@@ -5,25 +5,79 @@
  */
 function linkedListGenerator(){
 
+   var head = null;
+   var tail = null;
+
+   function getHead(){
+      return head;
+    }
+
+    function getTail(){
+      return tail;
+    }
+
+    function add(book){
+      var newNode = {
+        value: book,
+        next: null
+        };
+
+      if(head === null & tail === null){
+        head = newNode;
+        tail = head;
+
+      } else {
+        tail.next = newNode;
+        tail = newNode;
+      }
+
+      return  newNode;
+
+    }
+
+    function remove(n){
+        //removing node
+        if(get(n) === false){
+          return false;
+        }
+        else if(get(n) === getTail()){
+          get(n-1).next = null;
+          tail = get(n-1);
+        } else{
+            var nodeToRemove = get(n);
+            var preNode = get(n-1);
+            preNode.next = nodeToRemove.next;
+          }
+       }
+
+    function get(n){
+        var count = 0;
+        var currentObject = head;
+
+        while(currentObject !== null){
+          if(count === n){
+            console.log('current object',currentObject);
+            return currentObject;
+          }
+
+        count++;
+        currentObject = currentObject.next;
+      }
+      return false;
+    }
+
+    function insert(){}
+
+
+
+
    return {
-    getHead: function(){
-      return {
-        value: value,
-        next: {}
-      };
-    },
-    getTail: function(){},
-    add: function(){},
-    remove: function(){},
-    get: function(){},
-    insert: function(){},
+    getHead: getHead,
+    getTail: getTail,
+    add: add,
+    remove: remove,
+    get: get,
+    insert: insert
+  };
 
-      };
-
-
-
-
-
-};
-
-var books = linkedListGenerator();
+}
